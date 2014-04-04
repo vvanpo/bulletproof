032fe5f11aa1e3b5d3a5e4e5f8eb710d1a840ae3
Victor van Poppelen <vvanpo@gmail.com>
2014-04-04 17:55:12 -0400

    Removed unnecessary Path type, added yaml marshalling to config

--- ftracker.go
+++ ftracker.go
@@ -3,29 +3,21 @@ package main
 import (
 	"code.google.com/p/go.exp/fsnotify"
 	"crypto/md5"
+	"fmt"
 	"gopkg.in/yaml.v1"
-	"io"
+	"log"
+	"io/ioutil"
 	"os"
 	"path"
 	"strings"
 	"time"
 )
 
-// Directory and file name components
-//   e.g. {"home", "victor", "my-tracked-dir", "my-file"}
-//     or {"..", "my-tracked-dir", "my-file"}
-type Path []string
-
-// String representation of Path
-func (p Path) String() string {
-	return path.Join(p...)
-}
-
 // A representation of an inode, i.e. a unique file object
 type node struct {
-	size    []int64 // Size in bytes
+	size    int64 // Size in bytes
 	mode    os.FileMode
-	modtime time.Time
+	modTime time.Time
 	seLinux string         // SELinux context, if one exists
 	hash    [md5.Size]byte // MD5 hash of file data
 	links   []*link        // Back-references to files.  This is ignored if node is a directory
@@ -45,6 +37,15 @@ func (l link) isSymlink() bool {
 	return false
 }
 
+func (l *link) GetYAML() (string, interface{}) {
+	value := make(map[string]interface{})
+	value["Size"] = l.node.size
+	value["Hash"] = fmt.Sprintf("%x", l.node.hash)
+	value["Modified"] = l.node.modTime
+	// TODO:  file mode
+	return "", value
+}
+
 type dir struct {
 	// A directory is also just a link, but most systems don't support multi-linking directories and neither will we
 	name string
@@ -52,38 +53,60 @@ type dir struct {
 	// List of files
 	files []link
 	// List of symlinks within this directory, mapping the symlink files themselves to a pathname
-	symlinks []map[link]Path
+	symlinks map[link]string
 	// List of directories nested within this directory
 	dirs []dir
 }
 
+func (d *dir) GetYAML() (string, interface{}) {
+	value := make(map[string]interface{})
+	for _, l := range d.files {
+		value[l.name] = l
+	}
+	for l, v := range d.symlinks {
+		value[l.name] = []interface{}{"-> " + v, l}
+	}
+	for _, ds := range d.dirs {
+		value[ds.name] = []interface{}{ds.node, ds}
+	}
+	return "", value
+}
+
 // Per-instance file tree object
 type Instance struct {
+	// Absolute pathname to root
+	path string
 	// Root directory
 	root *dir
-	// Absolute pathname to root
-	path Path
 	// Absolute pathname mapping to fsnotify watcher objects
 	watchers map[string]fsnotify.Watcher
 }
 
-// NewInstance sets up the data structure for the given path by reading the configuration files
-func NewInstance(p Path) *Instance {
-	i := new(Instance)
-	i.root = new(dir)
-	b, err := yaml.Marshal(i)
-	print(err)
-	os.Create("example.yaml")
+func (i *Instance) GetYAML() (string, interface{}) {
+	tag, value := i.root.GetYAML()
+	return tag, value
+}
 
+// NewInstance sets up the data structure for the given pathname by reading the configuration files in that path
+func NewInstance(path string) *Instance {
+	i := new(Instance)
+	i.path = path
+	i.root = new(dir)
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Print(err)
+	}
+	var cfg map[string]interface{}
+	err = yaml.Unmarshal(b, &cfg)
+	if err != nil {
+		log.Print(err)
+	}
+	fmt.Printf("%#v\n", cfg)
 	return i
 }
 
-func (i *Instance) AddPath(p Path) error {
-	return nil
-}
-
 func main() {
-	root := strings.Split(os.Args[1], "/")
+	root := "/home/victor"
 	i := NewInstance(root)
 	println(i)
 }
