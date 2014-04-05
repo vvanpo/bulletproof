7ec5ce93c7ff35f8fb25e8a73fa456ffe67560a0
Victor van Poppelen <vvanpo@gmail.com>
2014-04-05 16:19:23 -0400

    Removed gcfg config parsing 	- No support for writing format or storing comments for write-consistency 	- No support for lists of data rather than key=value pairs Added config_test file

--- config.go
+++ config.go
@@ -2,21 +2,14 @@
 package filetracker
 
 import (
-	"code.google.com/p/gcfg"
 	"path/filepath"
 )
 
-// Filled with gcfg-parsed configuration file (code.google.com/p/gcfg)
-type config struct {
-	Sync map[string]*struct{
-		v []string
-	}
-	Local map[string]*struct{}
+type Config struct {
 }
 
-func GetConfig(root string) (*config, error) {
+func GetConfig(root string) (*Config, error) {
 	pathname := filepath.Join(root, ".bp/config")
-	c := new(config)
-	err := gcfg.ReadFileInto(c, pathname)
+	c := new(Config)
 	return c, err
 }
---
+++ config_test.go
@@ -0,0 +1,17 @@
+package filetracker
+
+import (
+	//"os"
+	"testing"
+)
+
+func TestGetConfig(t *testing.T) {
+//	pathname, _ := os.Getwd()
+	pathname := "/home/victor"
+	t.Logf("Using dir '%s'", pathname)
+	c, err := GetConfig(pathname)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Logf("Configuration:\n%v", c)
+}
