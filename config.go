a87bf251e8779d51ab0ba321d50c0c7852193f73
Victor van Poppelen <vvanpo@gmail.com>
2014-04-10 23:32:04 -0400

    No more config.go

--- config.go
+++ config.go
@@ -1,24 +1,8 @@
 package bp
 
 import (
-	"code.google.com/p/go-sqlite/go1/sqlite3"
-	"fmt"
-	"path/filepath"
 )
 
 func (s *Session) updateConf() error {
-	db := filepath.Join(s.root, ".bp/objects.db")
-	c, err := sqlite3.Open(db)
-	defer c.Close()
-	if err != nil {
-		return err
-	}
-	sql := "SELECT path FROM global"
-	row := make(sqlite3.RowMap)
-	for i, err := c.Query(sql); err == nil; err = i.Next() {
-		var path string
-		i.Scan(path, row)
-		fmt.Println(path, row)
-	}
 	return nil
 }
--- session.go
+++ session.go
@@ -13,8 +13,8 @@ type Session struct {
 	root string
 	// File watcher instance
 	watcher *fsnotify.Watcher
-	// List of watched paths
-	watch []string
+	// Map of all watched paths to events
+	event map[string]*fsnotify.FileEvent
 }
 
 func NewSession(root string) *Session {
@@ -31,6 +31,17 @@ func NewSession(root string) *Session {
 // Convenience method to return absolute pathname
 func (s *Session) absolutePath(path string) string {
 	return filepath.Join(s.root, path)
+}
+
+func (s *Session) start() error {
+	c, err := dbConn()
+	if err != nil { return err }
+	q, err := c.Query("SELECT path FROM folder")
+	folders := make([]string, 10)
+	for ; err == nil; err = q.Next() {
+
+	}
+	// err = filepath.Walk(s.root, walk)
 }
 
 func (s *Session) addWatch(path string) error {
